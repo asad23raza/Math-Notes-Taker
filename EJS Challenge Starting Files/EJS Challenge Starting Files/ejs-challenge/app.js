f89afdcc6d98440a646bfcc5a005cf20388c7060
asad23raza <a48raza@uwaterloo.ca>
2022-03-28 23:54:43 -0400

    add compose page

--- EJS Challenge Starting Files/EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/EJS Challenge Starting Files/ejs-challenge/app.js
@@ -10,6 +10,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const lorumIpsum = [homeStartingContent, aboutContent, contactContent]
 const app = express();
+var postEntries = []
 
 app.set('view engine', 'ejs');
 
@@ -17,8 +18,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render('home', {fillerText:lorumIpsum});
+  res.render('home', {fillerText:lorumIpsum, pst:postEntries});
 });
+app.get("/contact", function(req, res){
+  res.render('contact', {fillerText:lorumIpsum});
+});
+app.get("/about", function(req, res){
+  res.render('about', {fillerText:lorumIpsum});
+});
+app.get("/compose", function(req, res){
+  res.render('compose');
+});
+
+app.post("/compose", function(req, res){
+  post = {
+    title: req.body.newNote,
+    body: req.body.NoteBody
+  }
+  console.log(post)
+  postEntries.push(post)
+  res.redirect("/")
+})
+app.post("/", function(req, res){
+  console.log("delete")
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
